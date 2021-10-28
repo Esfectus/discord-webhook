72b8d5823f31124c02d28dcb00768303a875907d
Esfectus <esfectus@gmail.com>
2021-10-28 19:10:40 +0530

    add url variable for every method

--- discord-webhook/_test_main.py
+++ discord-webhook/_test_main.py
@@ -9,62 +9,62 @@ class Webhook:
         self._baseurl = 'https://discord.com/api/v9'
         self._auth_header = {'Authorization': f'Bot {token}'}
 
-    async def create_webhook(self):
+    async def create_webhook(self, channel_id):
         """Creates a new webhook.
         Create a new webhook. Requires the MANAGE_WEBHOOKS permission. Returns a webhook object on success.
         """
-        pass
+        url = self._baseurl+f'/channels/{channel_id}/webhooks'
 
-    async def get_channel_webhooks(self):
+    async def get_channel_webhooks(self, channel_id):
         """Create a new webhook. Requires the MANAGE_WEBHOOKS permission. Returns a webhook object on success."""
-        pass
+        url = self._baseurl+f'/channels/{channel_id}/webhooks'
 
-    async def get_guild_webhooks(self):
+    async def get_guild_webhooks(self, guild_id):
         """Returns a list of guild webhook objects. Requires the MANAGE_WEBHOOKS permission."""
-        pass
+        url = self._baseurl+f'/guilds/{guild_id}/webhooks'
 
-    async def get_webhook(self):
+    async def get_webhook(self, webhook_id):
         """Returns the new webhook object for the given id."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}'
 
-    async def get_webhook_with_token(self):
+    async def get_webhook_with_token(self, webhook_id, webhook_token):
         """Like get webhook except this call does not
          require authentication and returns no user in the webhook object."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}'
 
-    async def modify_webhook(self):
+    async def modify_webhook(self, webhook_id):
         """Modify a webhook. Requires the MANAGE_WEBHOOKS permission. Returns the updated webhook object on success."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}'
 
-    async def modify_webhook_with_token(self):
+    async def modify_webhook_with_token(self, webhook_id, webhook_token):
         """Same as above, except this call does not require authentication,
          does not accept a channel_id parameter in the body, and does not return a user in the webhook object."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}'
 
-    async def delete_webhook(self):
+    async def delete_webhook(self, webhook_id):
         """Delete a webhook permanently. Requires the MANAGE_WEBHOOKS permission.
          Returns a 204 NO CONTENT response on success."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}'
 
-    async def delete_webhook_with_token(self):
+    async def delete_webhook_with_token(self, webhook_id, webhook_token):
         """Same as delete webhook, except this call does not require authentication."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}'
 
-    async def execute_webhook(self):
+    async def execute_webhook(self, webhook_id, webhook_token):
         """Executes the webhook"""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}'
 
-    async def get_webhook_message(self):
+    async def get_webhook_message(self, webhook_id, webhook_token, message_id):
         """Returns a previously-sent webhook message from the same token. Returns a message object on success."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}'
 
-    async def edit_webhook_message(self):
+    async def edit_webhook_message(self, webhook_id, webhook_token, message_id):
         """Edits a previously-sent webhook message from the same token. Returns a message object on success."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}'
 
-    async def delete_webhook_message(self):
+    async def delete_webhook_message(self, webhook_id, webhook_token, message_id):
         """Deletes a message that was created by the webhook. Returns a 204 NO CONTENT response on success."""
-        pass
+        url = self._baseurl+f'/webhooks/{webhook_id}/{webhook_token}/messages/{message_id}'
 
 
 
